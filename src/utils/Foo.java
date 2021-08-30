e0370e9c71df762013f06f1d95e8679bd4a402ce
Hoa Le <hoa.nq.le3@gmail.com>
2021-08-30 08:34:55 -0400

    Added person2 greetings.

---
+++ src/utils/Foo.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'hoa1' branch, from Eclipse");
+		System.out.println("hello worlds");
+		System.out.println("hello from person2");
+	
+		
+	}
+
+
+}
